d1028ad11cefc35c326026a61a2bfe1dba74e740
PukinCAT <128095540+ApukinDEV@users.noreply.github.com>
2023-05-29 10:25:10 +0200

    zrobilem scrolla woooo

---
+++ Nowy folder/script.js
@@ -0,0 +1,27 @@
+function reset(id){
+    x=document.getElementById(id)
+    x.classList.remove("second")
+    x.classList.remove("third")
+    x.classList.remove("fourth")
+}
+function secondsetting(id){
+    x=document.getElementById(id)
+    x.classList.remove("second")
+    x.classList.remove("third")
+    x.classList.remove("fourth")
+    x.classList.add("second")
+}
+function thirdsetting(id){
+    x=document.getElementById(id)
+    x.classList.remove("second")
+    x.classList.remove("third")
+    x.classList.remove("fourth")
+    x.classList.add("third")
+}
+function fourthsetting(id){
+    x=document.getElementById(id)
+    x.classList.remove("second")
+    x.classList.remove("third")
+    x.classList.remove("fourth")
+    x.classList.add("fourth")
+}
